8f54044f563fcac98674731553558175e83943ad
Alchemist <blcklptn@gmail.com>
2023-02-01 16:22:46 +0500

    user_edit page connection

--- SourceFiles/enolemumiProject/globaladmin/urls.py
+++ SourceFiles/enolemumiProject/globaladmin/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns: list = [
     path('globaladmin', views.globaladminPage, name='globaladmin'),
-    path('globaladmin_statistic', views.globaladminStatistic, name='globaladmin')
+    path('globaladmin_statistic', views.globaladminStatistic, name='globaladmin'),
+    path('userEdit', views.userEdit, name='userEdit')
 ]
--- SourceFiles/enolemumiProject/globaladmin/views.py
+++ SourceFiles/enolemumiProject/globaladmin/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 def globaladminPage(request: object) -> render:
     if request.session['email']:
         print(request.session['email'])
-        return render(request, 'enolemumi/globalAdminPage.html', {'name': request.session['full_name']})
+        return render(request, 'enolemumi/globalAdminPage.html', {'name': request.session['full_name'], 'email': request.session['email']})
     else:
         return redirect('login')
 
 def globaladminStatistic(request: object) -> render:
     if request.session['email']:
-        return render(request, 'enolemumi/Statistic.html', {'name': request.session['full_name']})
+        return render(request, 'enolemumi/Statistic.html', {'name': request.session['full_name'], 'email': request.session['email']})
+
+
+def userEdit(request: object) -> render:
+    if request.session['email']:
+        return render(request, 'enolemumi/UserEdit.html', {'name': request.session['full_name'], 'email': request.session['email']}) 
