756c8c13107ddd630183dbc4fcfe669873466fe3
Alchemist <blcklptn@gmail.com>
2023-02-01 14:53:58 +0500

    admin panel

--- SourceFiles/enolemumiProject/login/views.py
+++ SourceFiles/enolemumiProject/login/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect
+from registration.models import MyUsers
 
 # Create your views here.
 def login(request: object) -> render:
     print('qweqwe')
     if request.method == 'POST':
-        print('autheficated')
+        email = request.POST['email']
+        password = request.POST['password']
+
+        userEx = MyUsers.objects.filter(email=email).exists()
+        if userEx:
+            user = MyUsers.objects.get(email=email)
+            if password == user.password:
+                print('Login!')
+                return redirect('/')
+            else:
+                return render(request, 'enolemumi/login.html')
+
+        else:
+            return render(request, 'enolemumi/login.html')
         return redirect('/')
+
     else:
         return render(request, 'enolemumi/login.html')
--- SourceFiles/enolemumiProject/registration/admin.py
+++ SourceFiles/enolemumiProject/registration/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MyUsers
 # Register your models here.
+@admin.register(MyUsers)
+class RequestDemoAdmin(admin.ModelAdmin):
+  list_display = ['fullName', 'email', 'password']
